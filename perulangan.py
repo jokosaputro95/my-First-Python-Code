02d0f51e3df7b1c9f7b187250eeee15ca2a5085f
Joko Saputro <jokosaputro616@gmail.com>
2022-11-24 18:17:47 +0700

    Perulangan while lanjutan!

--- perulangan.py
+++ perulangan.py
@@ -15,6 +15,7 @@ print(f'Jumlah buku yang sudah dibaca {jumlahBukuBaca}')
 """
 
 # Perulangan dengan while
+"""
 jumlahBuku = 10
 print('Ibu berkata, "Baca semua buku"')
 jumlahBukuBaca = 0
@@ -25,3 +26,29 @@ while jumlahBukuBaca < jumlahBuku:
     print(f"Baca ke {jumlahBukuBaca} buku")
 
 print(f'Jumlah buku yang sudah dibaca {jumlahBukuBaca}')
+"""
+
+# Perulangan dengan while with undefined
+"""
+Baca semua buku sampai faham
+"""
+jumlahBuku = 10
+print('Ibu berkata, "Baca semua buku sampai faham"')
+jumlahBukuBaca_dan_yangSudahdipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami sebanyak {jumlahBukuBaca_dan_yangSudahdipahami}')
+totalJumlahBaca = 0
+
+while totalJumlahBaca < jumlahBuku * 2:
+    totalJumlahBaca += 1
+    if jumlahBukuBaca_dan_yangSudahdipahami == 9:
+        print(f'Buku ke {jumlahBukuBaca_dan_yangSudahdipahami + 1} belum paham')
+    else:
+        jumlahBukuBaca_dan_yangSudahdipahami += 1
+        print(f'Buku ke {jumlahBukuBaca_dan_yangSudahdipahami} sudah dibaca dan dipahami')
+
+print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlahBukuBaca_dan_yangSudahdipahami}')
+
+if jumlahBukuBaca_dan_yangSudahdipahami == totalJumlahBaca:
+    print('"Bu, semua buku sudah dibaca dan dipahami"')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami, Budi hanya bisa memahami {jumlahBukuBaca_dan_yangSudahdipahami} buku"')
