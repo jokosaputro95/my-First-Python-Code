255555e09f8953d95ebba03c91bb0729bd27654d
Joko Saputro <jokosaputro616@gmail.com>
2022-11-24 16:19:19 +0700

    Sequential and Branching

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ print("My name is Joko Saputro")
 # Sekuensial
 print('Ibu berkata, "Pergi ke toko"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan ditoko?"')
-print('Ibu menjawab. "Beli satu botol susu, satu bungkus santan dan jika ada telur beli 6"')
+print('Ibu menjawab. "Beli satu botol susu, jika ada telur beli 6 butir"')
 print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlahBotolSusu = 120
+jumlahTelur = 1200
+
+print(f'Jumlah botol susu {jumlahBotolSusu} botol')
+print(f'Jumlah telur {jumlahTelur} butuir')
+
+if jumlahBotolSusu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlahTelur != 0:
+        print("Budi membeli 1 botol susu, dan 6 butir telur")
+else:
+    print("Budi tidak jadi membeli 1 botol susu, dan 6 butir telur")
+
+print("Budi pulang kerumah")
+print("Menyampaikan hasilnya kepada ibu")
