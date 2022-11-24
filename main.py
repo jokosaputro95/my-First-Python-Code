39188820db6c5cafaf285544fb25699169011d98
Joko Saputro <jokosaputro616@gmail.com>
2022-11-24 15:41:17 +0700

    my First Code with Python!

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ Ini adalah demo project pertama saya dengan python
 '''"""
 print("Hello world")
 print("My name is Joko Saputro")
-print("Hello world")
+
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan ditoko?"')
+print('Ibu menjawab. "Beli satu botol susu, satu bungkus santan dan jika ada telur beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
