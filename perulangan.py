19f1bf03e9b99a8e41d9e39b8aae0cd79092df10
Joko Saputro <jokosaputro616@gmail.com>
2022-11-24 18:21:52 +0700

    Perbaiki penamaan variabel

--- perulangan.py
+++ perulangan.py
@@ -34,21 +34,21 @@ Baca semua buku sampai faham
 """
 jumlahBuku = 10
 print('Ibu berkata, "Baca semua buku sampai faham"')
-jumlahBukuBaca_dan_yangSudahdipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami sebanyak {jumlahBukuBaca_dan_yangSudahdipahami}')
-totalJumlahBaca = 0
+jumlahBacaDanPaham = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami sebanyak {jumlahBacaDanPaham}')
+jumlahBaca = 0
 
-while totalJumlahBaca < jumlahBuku * 2:
-    totalJumlahBaca += 1
-    if jumlahBukuBaca_dan_yangSudahdipahami == 9:
-        print(f'Buku ke {jumlahBukuBaca_dan_yangSudahdipahami + 1} belum paham')
+while jumlahBaca < jumlahBuku * 2:
+    jumlahBaca += 1
+    if jumlahBacaDanPaham == 9:
+        print(f'Buku ke {jumlahBacaDanPaham + 1} belum paham')
     else:
-        jumlahBukuBaca_dan_yangSudahdipahami += 1
-        print(f'Buku ke {jumlahBukuBaca_dan_yangSudahdipahami} sudah dibaca dan dipahami')
+        jumlahBacaDanPaham += 1
+        print(f'Buku ke {jumlahBacaDanPaham} sudah dibaca dan dipahami')
 
-print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlahBukuBaca_dan_yangSudahdipahami}')
+print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlahBacaDanPaham}')
 
-if jumlahBukuBaca_dan_yangSudahdipahami == totalJumlahBaca:
+if jumlahBacaDanPaham == jumlahBaca:
     print('"Bu, semua buku sudah dibaca dan dipahami"')
 else:
-    print(f'"Bu, tidak semua buku bisa dipahami, Budi hanya bisa memahami {jumlahBukuBaca_dan_yangSudahdipahami} buku"')
+    print(f'"Bu, tidak semua buku bisa dipahami, Budi hanya bisa memahami {jumlahBacaDanPaham} buku"')
